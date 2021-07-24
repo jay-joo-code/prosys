367193a34a9a2f7390a015de259fba20baa5f41a
Jay Joo <jj534@cornell.edu>
2021-07-25 08:59:38 +0900

    Improve no cards left UI

--- client/src/pages/spaced-rep/Learn.tsx
+++ client/src/pages/spaced-rep/Learn.tsx
@@ -2,8 +2,10 @@ import FilterNoneOutlinedIcon from '@material-ui/icons/FilterNoneOutlined'
 import React from 'react'
 import { useRepCards } from 'src/api/card'
 import theme from 'src/app/theme'
+import { ReactComponent as IllustDone } from 'src/assets/illust-done.svg'
 import CardItem from 'src/components/card/CardItem'
 import Text from 'src/components/fonts/Text'
+import { FlexColumn } from 'src/components/layout/Flex'
 import styled from 'styled-components'
 
 const Learn = () => {
@@ -11,23 +13,26 @@ const Learn = () => {
 
   return (
     <Container>
+      <CardsCounter>
+        <FilterNoneOutlinedIcon />
+        <Text variant='p' color={theme.text.light}>
+          {cards?.length} remaining
+        </Text>
+      </CardsCounter>
       {cards && cards.length > 0 ? (
-        <>
-          <CardsCounter>
-            <FilterNoneOutlinedIcon />
-            <Text variant='p' color={theme.text.light}>
-              {cards?.length} remaining
-            </Text>
-          </CardsCounter>
-          <CardItem
-            card={cards[0]}
-            initStatus='EXPANDED'
-            isLearning
-            refetchCards={refetchCards}
-          />
-        </>
+        <CardItem
+          card={cards[0]}
+          initStatus='EXPANDED'
+          isLearning
+          refetchCards={refetchCards}
+        />
       ) : (
-        <Text variant='p'>Done for today!</Text>
+        <FlexColumn alignCenter>
+          <StyledIllustDone />
+          <Text variant='p' fontWeight={700} color={theme.text.muted}>
+            Completed all reps for today!
+          </Text>
+        </FlexColumn>
       )}
     </Container>
   )
@@ -46,6 +51,17 @@ const CardsCounter = styled.div`
 
   & svg {
     fill: ${(props) => props.theme.text.light} !important;
+  }
+`
+
+const StyledIllustDone = styled(IllustDone)`
+  width: 80%;
+  height: auto;
+  margin-top: 6rem;
+  margin-bottom: 2rem;
+
+  @media (min-width: ${(props) => props.theme.tablet}) {
+    width: 300px;
   }
 `
 
