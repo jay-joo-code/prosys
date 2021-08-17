ba19854ed84be75a698999b464b4e0341e2c0232
Jay Joo <jj534@cornell.edu>
2021-08-17 19:28:00 +0900

    Change saved uid

--- server/testScript.ts
+++ server/testScript.ts
@@ -1,11 +1,6 @@
 // import Task from './models/Task'
+// import User from './models/User'
 
 export default async () => {
-  // const JASON_UID_PROD = '60b43675702bb67d157913bc'
-  // const JASON_UID_DEV = '60b056530dd0935cd6c44fb4'
-  // const tasks = await Task.find({
-  //   userId: JASON_UID_DEV,
-  //   provider: undefined,
-  // })
-  // console.log('tasks', tasks)
+  // const CORNELL_UID_PROD = '60a9fa062bfdca0004875ece'
 }
